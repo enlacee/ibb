cd5ec859053f66d589b287306dc1bd2db2e7e39a
Anibal Copitan <acopitan@gmail.com>
2014-03-09 15:17:37 -0500

    module basic Persona

--- config/application.config.php
+++ config/application.config.php
@@ -4,6 +4,7 @@ return array(
     'modules' => array(
         'Application',
         'ZendSkeletonModule',
+        'Persona',
     ),
 
     // These are various options for the listeners attached to the ModuleManager
